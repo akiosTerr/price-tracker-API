fc01efab46c50d72ef897137a2d29ed8406e88a1
akios dev <akiosdev6@gmail.com>
2020-06-23 14:05:43 -0300

    cleaning the code

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,7 +1,5 @@
-const knex = require('knex');
 const environment = process.env.NODE_ENV || 'development';
 const config = require('../../knexfile')[environment];
-
-const connection = knex(config);
+const connection = require('knex')(config);
 
 module.exports = connection;
